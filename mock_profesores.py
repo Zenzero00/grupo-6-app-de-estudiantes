258ed07f95d2648014b03840b8b9cdde1eb43517
Samubetarock <samuelgonzalez164@gmail.com>
2024-02-22 22:17:12 -0400

    mock 101

---
+++ mock_profesores.py
@@ -0,0 +1,27 @@
+# LISTA DE PROFESORES BY SAM
+
+profesores = [
+    {
+        "nombre": "Pedro Pascal",
+        "dni": "111111111",
+        "correo": "pedro@bar.com",
+        "aula": "Aula Virtual 1"
+    },
+    {
+        "nombre": "María Virginia",
+        "dni": "2222222",
+        "correo": "maria@qux.com",
+        "aula": "Aula Virtual 2"
+    },
+    {
+        "nombre": "Juan Bozo",
+        "dni": "33333333",
+        "correo": "juan@foo.com",
+        "aula": "Aula Virtual 3"
+    }
+]
+
+
+# print("Nombre: ", profesores[0]["nombre"])
+# print("DNI: ", profesores[0]["dni"])
+# print("Correo: ", profesores[0]["correo"])
