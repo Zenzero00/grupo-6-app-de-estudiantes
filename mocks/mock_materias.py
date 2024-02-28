a3fa1c9cba8e68156c257236e3b7c189f359fe2d
MagaDev <magahr123@gmail.com>
2024-02-27 23:10:23 -0400

    feature-103

---
+++ mocks/mock_materias.py
@@ -0,0 +1,32 @@
+materias = [ {
+  "id": "1",
+  "name": "Matematica",
+  "horario": "08-09am",
+  "dni_profesor": "33333333"
+},
+{
+  "id": "2",
+  "name": "Ingles",
+  "horario": "09-10am",
+  "dni_profesor": "111111111"
+},
+{
+  "id": "3",
+  "name": "Programacion",
+  "horario": "11-12m",
+  "dni_profesor": "33333333"
+},
+{
+  "id": "4",
+  "name": "Logica",
+  "horario": "13-14pm",
+  "dni_profesor": "33333333"
+},
+{
+  "id": "5",
+  "name": "Computacion",
+  "horario": "15-16pm",
+  "dni_profesor": "2222222"
+},
+]
+print(materias)
